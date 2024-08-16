613b9f716271bb17c584198d7d4c90e57d75d9fb
Bao Minh Doan Dang <minh.ddb@gmail.com>
2024-08-16 10:11:53 +0200

    Conditions to mask all context and remove whole sequence added

--- masking/masking.py
+++ masking/masking.py
@@ -26,23 +26,27 @@ class DatasetMasking(Data):
         masks_dataset = dict(tokens=[], ner_tags=[])
         entity_sequences = [seq["tokens"] for seq in self.get_entity_sequences(tag_name=self.tag_name)]
         random.seed(p_mask)
-        if strategy == "no_mask":
-            p_mask = 0.0
-        sequences_for_masking = random.sample(entity_sequences, math.floor(len(entity_sequences) * p_mask))
+        random_samples = random.sample(entity_sequences, math.floor(len(entity_sequences) * p_mask))
         for sequence, tags in self.sequence_generator():
-            if sequence in sequences_for_masking:
+            if sequence in random_samples:
                 sequence_mask = SequenceMasking(sequence, tags, mask_token)
                 if strategy == "entity":
                     for masked in sequence_mask.mask_entity():
                         sequence = masked
-                elif strategy == "windows":
+                if strategy == "windows":
                     for masked in sequence_mask.mask_context(windows_size=windows_size):
                         sequence = masked
-                else:
+                if strategy == "outside":
                     for masked in sequence_mask.mask_context(windows_size=windows_size, mask_windows=False):
                         sequence = masked
-            masks_dataset["tokens"].append(sequence)
-            masks_dataset["ner_tags"].append(tags)
+                if strategy == "all_context":
+                    for masked in sequence_mask.mask_context(mask_windows=False, mask_all=True):
+                        sequence = masked
+                if strategy == "remove":
+                    sequence = [mask_token for _ in range(len(sequence))]
+            if not all(token == mask_token for token in sequence):
+                masks_dataset["tokens"].append(sequence)
+                masks_dataset["ner_tags"].append(tags)
         return datasets.Dataset.from_dict(masks_dataset)
 
 
@@ -65,9 +69,10 @@ class SequenceMasking:
             for i, token in enumerate(self.sequence)
         ]
 
-    def mask_context(self, windows_size: int = 1, mask_windows: bool = True):
+    def mask_context(self, windows_size: int = 1, mask_windows: bool = True, mask_all: bool = False):
         """
         Mask contextual information within the given tokens sequence
+        :param mask_all:
         :param windows_size: Size of context windows
         :param mask_windows: Boolean indicator whether tokens from context windows should be masked.
         Mask if True, else mask other contextual tokens
@@ -84,15 +89,19 @@ class SequenceMasking:
                 for i, token in enumerate(self.sequence)
             ]
         else:
-            # Mask all contextual tokens that are not contained in list of context windows
-            if context_windows_tokens_ids:
-                yield [
-                    token if i in self.entity_tokens_ids or i in context_windows_tokens_ids
-                    else self.mask_token
-                    for i, token in enumerate(self.sequence)
-                ]
+            if not mask_all:
+                # Mask all contextual tokens that are not contained in list of context windows
+                if context_windows_tokens_ids:
+                    yield [
+                        token if i in self.entity_tokens_ids or i in context_windows_tokens_ids
+                        else self.mask_token
+                        for i, token in enumerate(self.sequence)
+                    ]
+                else:
+                    yield self.sequence
             else:
-                yield self.sequence
+                # Mask all context tokens
+                yield [self.mask_token if i not in self.entity_tokens_ids else token for i,token in enumerate(self.sequence)]
 
 
 if __name__ == "__main__":
@@ -104,8 +113,9 @@ if __name__ == "__main__":
 
     sequence_masking = SequenceMasking(tokens, tags)
 
-    masked_context = sequence_masking.mask_context(windows_size=4)
+    masked_context_windows = sequence_masking.mask_context(windows_size=4)
+    masked_all_context = sequence_masking.mask_context(mask_windows = False, mask_all=True)
     masked_entity = sequence_masking.mask_entity()
 
-    for s, t, m, me in zip(tokens, tags, masked_context, masked_entity):
-        print(s, t, m, me, sep="\t")
+    for s, t, mcw, mac, me in zip(tokens, tags, masked_context_windows, masked_all_context, masked_entity):
+        print(s, t, mcw, mac, me, sep="\n")
